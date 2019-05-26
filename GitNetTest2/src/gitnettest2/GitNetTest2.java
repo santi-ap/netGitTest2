142d13708985a4bde62f8ad228579a97b9dc9c33
Santiago <santialfonso@192.168.1.119>
2019-05-25 23:38:33 -0600

    committing some more

--- GitNetTest2/src/gitnettest2/GitNetTest2.java
+++ GitNetTest2/src/gitnettest2/GitNetTest2.java
@@ -17,6 +17,7 @@ public class GitNetTest2 {
     public static void main(String[] args) {
         // TODO code application logic here
         //testing testing
+        //testing sum more
     }
     
 }
